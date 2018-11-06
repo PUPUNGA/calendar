bb129a2653af3fbe78656dc9c0d9b3caca830b7e
sky <sky@sky>
2018-11-06 16:00:12 +0900

    update sc는 끄도록하자

---
+++ src/gitCalendar/Sum.java
@@ -0,0 +1,21 @@
+package gitCalendar;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.nextLine();
+		
+		String[] b = a.split(",");
+		
+		int num1 = Integer.parseInt(b[0]);
+		int num2 = Integer.parseInt(b[1]);
+		
+		System.out.println(num1+num2);
+		sc.close();
+	}
+
+}
